978e819fb78b57bb22daf4689b36a26460220e35
Chris <chris.lai@appit.com.hk>
2024-07-18 19:41:33 +0800

    HomePageProductSection

--- src/assets/images/home-page/index.tsx
+++ src/assets/images/home-page/index.tsx
@@ -1,9 +1,11 @@
+import ImageHomePageGradientBanner1 from "./home-page-gradient-banner-1.png";
 import ImageHomePageServiceCard1 from "./home-page-service-card-1.png";
 import ImageHomePageServiceCard2 from "./home-page-service-card-2.png";
 import ImageHomePageServiceCard3 from "./home-page-service-card-3.png";
 import ImageHomePageServiceCard4 from "./home-page-service-card-4.png";
 
 export const homePageImages = {
+    ImageHomePageGradientBanner1,
     ImageHomePageServiceCard1,
     ImageHomePageServiceCard2,
     ImageHomePageServiceCard3,
--- src/pages/homePageProductSection.tsx
+++ src/pages/homePageProductSection.tsx
@@ -1,5 +1,9 @@
+import { Images } from "@/assets/images";
 import { ImiDescSection } from "@/components/imiDescSection";
+import { ImiGradientBanner } from "@/components/imiGradientBanner";
+import { ContainerX } from "@/components/layout/containerX";
 import { i18nHelper } from "@/utils/i18n-helper";
+import "./_homePage.scss";
 
 export const HomePageProductSection = () => {
     const t = i18nHelper("home-page");
@@ -8,6 +12,24 @@ export const HomePageProductSection = () => {
         <ImiDescSection
             header={t("product.header")}
             desc={[t("product.desc")]}
+            customFullWidthBottomComponent={
+                <ContainerX>
+                    <div className="custom-bottom-container">
+                        <ImiGradientBanner
+                            colorTheme={"primary"}
+                            imgUrl={Images.ImageHomePageGradientBanner1}
+                            header={t("product.gradientBanner1.header")}
+                            subheader={t("product.gradientBanner1.subheader")}
+                            descList={[t("product.gradientBanner1.desc")]}
+                            button={{
+                                text: t("product.gradientBanner1.linkText"),
+                                type: "outline",
+                                colorTheme: "white",
+                            }}
+                        />
+                    </div>
+                </ContainerX>
+            }
         />
     );
 };
