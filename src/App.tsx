7439545965230fdd3d163ebdaebea27a7a42749a
Chris <chris.lai@appit.com.hk>
2024-07-22 17:55:57 +0800

    RemoveTrailingSlash

--- src/App.tsx
+++ src/App.tsx
@@ -3,48 +3,58 @@ import { ProtectedRoute } from "./ProtectedRoute";
 import { Layout } from "./components/layout";
 import { HomePage } from "./pages";
 import { Secret } from "./pages/Secret";
+import { PractitionersPage } from "./pages/practitioners";
 import { Services } from "./pages/services";
 import "./scss/main.scss";
+import { RemoveTrailingSlash } from "./utils/routes/RemoveTrailingSlash";
 import { RouteConstant } from "./utils/routes/route";
 
 function App() {
     return (
-        <Routes>
-            <Route index element={<HomePage />} />
+        <>
+            <RemoveTrailingSlash />
+            <Routes>
+                <Route index element={<HomePage />} />
 
-            <Route element={<Layout />}>
-                <Route
-                    path={RouteConstant.practitioners}
-                    element={<HomePage />}
-                ></Route>
-
-                <Route path={RouteConstant.services} element={<Services />}>
+                <Route element={<Layout />}>
                     <Route
-                        path={RouteConstant.naturopathicAndFunctionalMedicine}
+                        path={RouteConstant.practitioners}
+                        element={<PractitionersPage />}
+                    ></Route>
+
+                    <Route path={RouteConstant.services} element={<Services />}>
+                        <Route
+                            path={
+                                RouteConstant.naturopathicAndFunctionalMedicine
+                            }
+                            element={<HomePage />}
+                        />
+                    </Route>
+
+                    <Route
+                        path={RouteConstant.shop}
                         element={<HomePage />}
+                    ></Route>
+
+                    <Route
+                        path={RouteConstant.dispensary}
+                        element={<HomePage />}
+                    ></Route>
+
+                    <Route
+                        path={RouteConstant.healthHub}
+                        element={<HomePage />}
+                    ></Route>
+                    <Route
+                        element={
+                            <ProtectedRoute>
+                                <Route path={"secret"} element={<Secret />} />
+                            </ProtectedRoute>
+                        }
                     />
                 </Route>
-
-                <Route path={RouteConstant.shop} element={<HomePage />}></Route>
-
-                <Route
-                    path={RouteConstant.dispensary}
-                    element={<HomePage />}
-                ></Route>
-
-                <Route
-                    path={RouteConstant.healthHub}
-                    element={<HomePage />}
-                ></Route>
-                <Route
-                    element={
-                        <ProtectedRoute>
-                            <Route path={"secret"} element={<Secret />} />
-                        </ProtectedRoute>
-                    }
-                />
-            </Route>
-        </Routes>
+            </Routes>
+        </>
     );
 }
 
---
+++ src/pages/practitioners/index.tsx
@@ -0,0 +1,7 @@
+export const PractitionersPage = () => {
+    return (
+        <div style={{ margin: "80px", backgroundColor: "#c6c6c6" }}>
+            Practitioners page
+        </div>
+    );
+};
