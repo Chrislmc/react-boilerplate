632632f785ff1b63fe164ae96d111e33d04074f3
Chris <chris.lai@appit.com.hk>
2024-09-02 17:04:12 +0800

    InsurancePageDescSection

--- src/pages/others/insurance/insurancePageDescSection.tsx
+++ src/pages/others/insurance/insurancePageDescSection.tsx
@@ -4,5 +4,16 @@ import { i18nHelper } from "@/utils/i18n-helper";
 export const InsurancePageDescSection = () => {
     const t = i18nHelper("others");
 
-    return <ImiDescSection />;
+    return (
+        <ImiDescSection
+            header={t("insurance.header")}
+            subheader={t("insurance.subheader")}
+            desc={[
+                t("insurance.desc1"),
+                t("insurance.desc2"),
+                t("insurance.desc3"),
+                t("insurance.desc4"),
+            ]}
+        />
+    );
 };
