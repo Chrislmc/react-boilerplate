c956b32d121e27b7678c986b64a155bf3ad31749
Chris <chris.lai@appit.com.hk>
2024-09-04 17:12:57 +0800

    WIP

--- src/pages/others/corporate-wellness/corporateWellnessPageVideoSection.tsx
+++ src/pages/others/corporate-wellness/corporateWellnessPageVideoSection.tsx
@@ -1,17 +1,19 @@
 import { ImiVideoPlayer } from "@/components/imiVideoPlayer";
-import { convertRemToPx } from "@/utils/css-helper";
 
 export const CorporateWellnessPageVideoSection = () => {
     return (
-        <div className="video-container">
-            <ImiVideoPlayer
-                url={"https://www.youtube.com/watch?v=FkefRSxpuFk"}
-                muted={false}
-                controls={false}
-                height={convertRemToPx(37.5)}
-                width={"100%"}
-                loop
-            />
-        </div>
+        <>
+            <div className="video-container">
+                <ImiVideoPlayer
+                    url={"https://www.youtube.com/watch?v=FkefRSxpuFk"}
+                    muted={false}
+                    controls={false}
+                    height={"100%"}
+                    width={"auto"}
+                    loop
+                    className="react-player"
+                />
+            </div>
+        </>
     );
 };
