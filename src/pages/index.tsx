6ef3bc71d4253d40147e79f6de4f956782036e57
Chris <chrislmc25@gmail.com>
2024-09-19 18:26:05 +0800

    init components page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,22 @@
+import { DescSection } from "@/components/descSection";
+import { Section } from "@/components/section";
+import "./_components-page.scss";
+
 export const ComponentsPage = () => {
-    return <div id="components-page"></div>;
+    return (
+        <div id="components-page">
+            <Section className="components-page-desc-section">
+                <DescSection
+                    header="Components showcase page"
+                    desc={["Try out all the components!"]}
+                    customBottomComponent={
+                        <div className="custom-bottom-component">
+                            <div className="scroll-section"></div>
+                            <div className="content-section"></div>
+                        </div>
+                    }
+                />
+            </Section>
+        </div>
+    );
 };
