f65d279e823b8340df42a5ec13d8b662352a4c66
Chris <chrislmc25@gmail.com>
2024-10-29 15:52:38 +0800

    HomePageHeroBannerSection fade in

--- src/pages/homePageHeroBannerSection.tsx
+++ src/pages/homePageHeroBannerSection.tsx
@@ -3,15 +3,19 @@ import { SearchIconComponent } from "@/assets/icons/shared/search.component";
 import { Images } from "@/assets/images";
 import { Button } from "@/components/atoms/button";
 import { Input } from "@/components/atoms/input";
+import FadeIn from "@/components/fadeIn";
 import { ContainerX } from "@/components/layout/containerX";
+import useViewport from "@/utils/hooks/useViewport";
 import { i18nHelper } from "@/utils/i18n-helper";
+import { RefObject } from "react";
 import { contactMethods } from "./homePageConstant";
 
 export const HomePageHeroBannerSection = () => {
+    const [, currentElement] = useViewport<HTMLDivElement>(-200);
     const t = i18nHelper("home-page");
 
     return (
-        <ContainerX>
+        <ContainerX ref={currentElement as RefObject<HTMLDivElement>}>
             <div className="header-row">
                 <div className="icon-container">
                     <img src={Icons.IconNoBackground} onLoad={() => null} />
@@ -49,34 +53,42 @@ export const HomePageHeroBannerSection = () => {
                 </div>
             </div>
             <div className="hero-banner-container">
-                <div className="desc-container">
-                    <h1 className="header">
-                        I'M <p className="header-highlight">Chris Lai</p>
-                    </h1>
+                <FadeIn transitionDuration={800} delay={200} direction="right">
+                    <div className="desc-container">
+                        <h1 className="header">
+                            I'M <p className="header-highlight">Chris Lai</p>
+                        </h1>
 
-                    <h2 className="header-desc">Fullstack Developer</h2>
+                        <h2 className="header-desc">Fullstack Developer</h2>
 
-                    <Button text={"Contact Me"} type="outline-rectangle" />
-                </div>
+                        <Button text={"Contact Me"} type="outline-rectangle" />
+                    </div>
+                </FadeIn>
 
                 <div className="image-container">
                     <img src={Images.Portrait2} />
                 </div>
 
-                <div className="contacts-container">
-                    {contactMethods.map((method, i) => {
-                        const Icon = method.icon;
-                        return (
-                            <a
-                                key={`contact-method-${i}`}
-                                href={method.redirectUrl}
-                                target="_blank"
-                            >
-                                <Icon />
-                            </a>
-                        );
-                    })}
-                </div>
+                <FadeIn
+                    transitionDuration={800}
+                    delay={1000}
+                    className="contacts-container-fade-in"
+                >
+                    <div className="contacts-container">
+                        {contactMethods.map((method, i) => {
+                            const Icon = method.icon;
+                            return (
+                                <a
+                                    key={`contact-method-${i}`}
+                                    href={method.redirectUrl}
+                                    target="_blank"
+                                >
+                                    <Icon />
+                                </a>
+                            );
+                        })}
+                    </div>
+                </FadeIn>
             </div>
         </ContainerX>
     );
