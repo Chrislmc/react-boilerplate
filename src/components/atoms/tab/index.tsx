d3518743ecbcc22285fdc36318d88a569e9f26ef
Chris <chrislmc25@gmail.com>
2025-01-10 22:19:43 +0800

    add scrolling effect to tab

--- src/components/atoms/tab/index.tsx
+++ src/components/atoms/tab/index.tsx
@@ -1,4 +1,5 @@
-import { Dispatch, SetStateAction, useState } from "react";
+import { convertRemToPx } from "@/utils/css-helper";
+import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
 import "./_tab.scss";
 
 interface Props<T> {
@@ -17,10 +18,38 @@ export const Tab = <T extends string>({
     disabled,
 }: Props<T>) => {
     const [tabState, setTabState] = useState(activeTab || options[0].value);
+    const TabRef = useRef<HTMLDivElement>(null);
+
+    const activeIndex = options.findIndex(
+        (option) => option.value === tabState
+    );
+
+    const segmentGap = 6.25;
+
+    useEffect(() => {
+        /* in rem, gap between segments, refer to ./_tab.scss */
+        const scrollLeft =
+            options.slice(0, activeIndex).reduce(
+                (partialSum, reduceOption) =>
+                    partialSum +
+                        (
+                            document.getElementById(
+                                `tab-item-${reduceOption.value}`
+                            ) as HTMLElement
+                        ).offsetWidth || 0,
+
+                0
+            ) +
+            activeIndex * convertRemToPx(segmentGap);
+
+        setTimeout(() => {
+            TabRef.current?.scrollTo({ left: scrollLeft, behavior: "smooth" });
+        }, 200);
+    }, [activeIndex, options, segmentGap]);
 
     const onTabItemClick = (value: T) => {
         setTabState(value);
-
+        console.log("hi");
         if (onTabClick) {
             onTabClick(value);
             return;
@@ -30,13 +59,14 @@ export const Tab = <T extends string>({
     };
 
     return (
-        <div className="tab-component">
+        <div className="tab-component" ref={TabRef}>
             {options.map((option, i) => (
                 <div
                     key={`tab-${i}`}
                     className={`tab-item${
                         tabState === option.value ? " mod__active" : ""
                     }${disabled ? " mod__disabled" : ""}`}
+                    id={`tab-item-${option.value}`}
                 >
                     <button
                         onClick={() => onTabItemClick(option.value)}
